b60c597fc523f51d595f068fa51da2872f5cee88
David Jiang <davidjems@gmail.com>
2022-05-01 15:51:01 -0400

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const submitHandler = (event) =>{
+    event.preventDefault()
+    console.log("The form was submitted")
+}
 
+const main = () =>{
+    const form = document.querySelector("#park-form")
+    form.addEventListener("submit", submitHandler)
+    
+}
+
+window.addEventListener("DOMContentLoaded", main)
+
+const parkName = document.querySelector("#name-input").value;
+console.log(parkName)
